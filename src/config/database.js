e168c9ea3b0818e884b1670c4e9b9fa8c8d25d62
iNeZiiaaH <furiouse693@gmail.com>
2025-03-10 13:33:00 +0100

    feat(database): add automatic creation of MongoDB collections at startup

--- src/config/database.js
+++ src/config/database.js
@@ -6,10 +6,54 @@ const connectDB = async () => {
     try {
         await mongoose.connect(MONGO_URI);
         console.log("✅ MongoDB connecté !");
+
+        await ensureCollectionsExist();
+
     } catch (error) {
         console.error("❌ Erreur de connexion à MongoDB :", error);
         process.exit(1);
     }
 };
+
+const ensureCollectionsExist = async () => {
+    const User = require("../models/User");
+    const Room = require("../models/Room");
+    const Message = require("../models/Message");
+
+    // Cree la table et insert une donnee
+    let adminUser = await User.findOne();
+    if (!adminUser) {
+        adminUser = await User.create({
+            username: "admin",
+            email: "admin@example.com",
+            password: "admin",
+            role: "admin",
+            status: "approved"
+        });
+        console.log(" table User créé ");
+    }
+
+    // Cree la table et insert une donnee
+    if (!(await Room.exists({}))) {
+        await Room.create({
+            game: "TestGame",
+            createdBy: adminUser._id,
+            users: [adminUser._id]
+        });
+        console.log("table rooms créée ");
+    }
+
+    // Cree la table et insert une donnee
+    let testRoom = await Room.findOne();
+    if (!(await Message.exists({}))) {
+        await Message.create({
+            roomId: testRoom._id,
+            senderId: adminUser._id,
+            content: "test"
+        });
+        console.log("table messages créée ");
+    }
+};
+
 
 module.exports = connectDB;
