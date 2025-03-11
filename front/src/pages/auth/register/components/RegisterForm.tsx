cfbf4d94b54a15bcef6d4776bec97bb1d299cc19
Gwen <gwendoline.garlet@live.fr>
2025-03-11 13:50:26 +0100

    fest - Mise en place du service register

--- front/src/pages/auth/register/components/RegisterForm.tsx
+++ front/src/pages/auth/register/components/RegisterForm.tsx
@@ -14,8 +14,14 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import PasswordField from "./PasswordField";
 import formSchema from "../../utils/formSchema";
+import { useState } from "react";
+import authService from "@/services/authService";
+import { Toaster, toast } from "sonner";
+import { useNavigate } from "react-router-dom";
 
 const RegisterForm = () => {
+  const navigate = useNavigate();
+  const [authS] = useState(new authService());
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -27,85 +33,100 @@ const RegisterForm = () => {
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const resp = await authS.register(values);
+    if (resp && resp.status === 201) {
+      toast.success(
+        resp.data.message +
+          "Vous allez être rediriger vers la page de connexion !" ||
+          "Inscription réussie ! Vous allez être rediriger vers la page de connexion !"
+      );
+      setTimeout(() => {
+        navigate("/login");
+      }, 2000);
+    } else {
+      toast.error("Une erreur est survenue");
+    }
   }
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Pseudo</FormLabel>
-              <FormControl>
-                <Input type="text" placeholder="Nom" {...field} />
-              </FormControl>
-              <FormDescription>Pseudo</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input type="text" placeholder="Email" {...field} />
-              </FormControl>
-              <FormDescription>Email</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="motivation"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Motivation</FormLabel>
-              <FormControl>
-                <Input type="text" placeholder="Motivation" {...field} />
-              </FormControl>
-              <FormDescription>Motivation</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Mot de passe</FormLabel>
-              <FormControl>
-                <PasswordField showPassword={false} {...field} />
-              </FormControl>
-              <FormDescription>Mot de passe</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="checkPassword"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Confirmation du mot de passe</FormLabel>
-              <FormControl>
-                <PasswordField showPassword={false} {...field} />
-              </FormControl>
-              <FormDescription>Confirmation du mot de passe</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+    <div>
+      <Toaster position="top-right" richColors />
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Pseudo</FormLabel>
+                <FormControl>
+                  <Input type="text" placeholder="Nom" {...field} />
+                </FormControl>
+                <FormDescription>Pseudo</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input type="text" placeholder="Email" {...field} />
+                </FormControl>
+                <FormDescription>Email</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="motivation"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Motivation</FormLabel>
+                <FormControl>
+                  <Input type="text" placeholder="Motivation" {...field} />
+                </FormControl>
+                <FormDescription>Motivation</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Mot de passe</FormLabel>
+                <FormControl>
+                  <PasswordField showPassword={false} {...field} />
+                </FormControl>
+                <FormDescription>Mot de passe</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="checkPassword"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Confirmation du mot de passe</FormLabel>
+                <FormControl>
+                  <PasswordField showPassword={false} {...field} />
+                </FormControl>
+                <FormDescription>Confirmation du mot de passe</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
   );
 };
 
