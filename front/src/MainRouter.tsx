944b540f564f8a959a1c213c04d0fa68f8390c48
Gwen <gwendoline.garlet@live.fr>
2025-03-12 11:45:24 +0100

    fix - Ajout de l'index

--- front/src/MainRouter.tsx
+++ front/src/MainRouter.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
+import Login from "./pages/auth/login/Login";
 import Register from "./pages/auth/register/Register";
+import App from "./App";
 
 function MainRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<App />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
