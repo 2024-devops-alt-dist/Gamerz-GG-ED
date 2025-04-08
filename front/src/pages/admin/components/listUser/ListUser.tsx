b6da176af91bdf5e9ea3a6a3be35fb6e49b5553f
Gwen <gwendoline.garlet@live.fr>
2025-04-08 17:51:07 +0200

    Refacto - Suppression du composant ListUserPending ajout d'un type pour créer un variant de ListUser

--- front/src/pages/admin/components/listUser/ListUser.tsx
+++ front/src/pages/admin/components/listUser/ListUser.tsx
@@ -3,31 +3,66 @@ import DataTableUsers from "./DataTableUsers";
 import UserService from "@/services/UserService";
 import userI from "@/interfaces/userI";
 import { columns } from "./Column";
+import { ListUserType } from "@/interfaces/Iglobal";
 
-function ListUser() {
+interface ListUserProps {
+  type: ListUserType;
+}
+
+function ListUser({ type }: ListUserProps) {
   const [userService] = useState(new UserService());
   const [users, setUsers] = useState<userI[] | null>([]);
-
+  const [usersPending, setUsersPending] = useState<userI[] | null>([]);
+  const [statusSelections, setStatusSelections] = useState({});
+  const [banedSelections, setBanedSelections] = useState<
+    Record<string, boolean>
+  >({});
   async function getAllUsers() {
     try {
       const resp = await userService.getAll();
       const usersApproved = resp?.filter((user) => user.status === "approved");
-      if (usersApproved) setUsers(usersApproved);
+      if (usersApproved) {
+        setUsers(usersApproved);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async function getAllUsersByStatusPending() {
+    try {
+      const resp = await userService.getByStatusPending();
+      setUsersPending(resp);
     } catch (error) {
       console.log(error);
     }
   }
   useEffect(() => {
-    getAllUsers();
-  }, []);
-
+    if (type === "all") getAllUsers();
+    else if (type === "pending") getAllUsersByStatusPending();
+    else return;
+  }, [type]);
+  const columnsConfig = columns({
+    statusSelections,
+    setStatusSelections,
+    banedSelections,
+    setBanedSelections,
+  });
   return (
     <div className="container mx-auto py-10">
       <DataTableUsers
-        type={"approved"}
-        refreash={getAllUsers}
-        columns={columns()}
-        data={users ? users : []}
+        setBanedSelections={setBanedSelections}
+        banedSelections={banedSelections}
+        statusSelections={statusSelections}
+        setStatusSelections={setStatusSelections}
+        refreash={users ? getAllUsers : getAllUsersByStatusPending}
+        columns={columnsConfig}
+        data={
+          type === "all" && users
+            ? users
+            : type === "pending" && usersPending
+            ? usersPending
+            : []
+        }
       />
     </div>
   );
