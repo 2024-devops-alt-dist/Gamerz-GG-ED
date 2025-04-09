70c972bf8b3addff33cbe26ae586db57d3da2025
Gwen <gwendoline.garlet@live.fr>
2025-04-09 17:06:09 +0200

    Fix - Back deleteRoom pour accepter plusieur ids

--- back/src/routes/roomRoutes.js
+++ back/src/routes/roomRoutes.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const authMiddleware = require("../middlewares/authMiddleware");
 const isRoomOwner = require("../middlewares/isRoomOwner");
-const { createRoom, getRooms, getRoomById, joinRoom, leaveRoom, deleteRoom  } = require("../controller/roomController");
+const {
+  createRoom,
+  getRooms,
+  getRoomById,
+  joinRoom,
+  leaveRoom,
+  deleteRoom,
+} = require("../controller/roomController");
 
 const router = express.Router();
 
@@ -10,6 +17,6 @@ router.get("/", authMiddleware, getRooms);
 router.get("/:id", authMiddleware, getRoomById);
 router.post("/:id/join", authMiddleware, joinRoom);
 router.post("/:id/leave", authMiddleware, leaveRoom);
-router.delete("/:id", authMiddleware, isRoomOwner, deleteRoom);
+router.delete("/delete", authMiddleware, deleteRoom);
 
 module.exports = router;
