a52c14b42be543cdab726b5767707460c6032319
Gwen <gwendoline.garlet@live.fr>
2025-04-09 13:42:01 +0200

    Feat - mise en place du BaseService

---
+++ front/src/services/baseService.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+class BaseService<T> {
+  private API_URL: string;
+
+  constructor(private pathUrl: string) {
+    this.API_URL = `${import.meta.env.VITE_API_URL}${this.pathUrl}`;
+  }
+
+  async create(data: T) {
+    const resp = await axios.post(`${this.API_URL}`, data, {
+      withCredentials: true,
+    });
+    return resp;
+  }
+
+  async getAll() {
+    const resp = await axios.get(`${this.API_URL}`, {
+      withCredentials: true,
+    });
+    return resp;
+  }
+}
+export default BaseService;
---
+++ front/src/services/roomService.ts
@@ -0,0 +1,10 @@
+import IRoom from "@/interfaces/IRoom";
+import BaseService from "./baseService";
+
+class RoomService extends BaseService<IRoom> {
+  constructor() {
+    super("rooms");
+  }
+}
+
+export default RoomService;
