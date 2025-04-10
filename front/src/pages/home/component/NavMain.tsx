f370f769a5d55e0440ed9bcd3b5c4eb7cfda5411
Gwen <gwendoline.garlet@live.fr>
2025-04-10 09:30:37 +0200

    Fix - Récupere seulement les salons associer au user connecter

--- front/src/pages/home/component/NavMain.tsx
+++ front/src/pages/home/component/NavMain.tsx
@@ -1,52 +1,58 @@
 "use client";
 
-import {Gamepad2} from "lucide-react";
+import { Gamepad2 } from "lucide-react";
 import {
-    SidebarGroup,
-    SidebarGroupLabel,
-    SidebarMenu,
-    SidebarMenuButton,
-    SidebarMenuItem,
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from "@/components/ui/sidebar.tsx";
-import {useEffect, useState} from "react";
-import roomI from "@/interfaces/roomI";
+import { useContext, useEffect, useState } from "react";
 import RoomService from "@/services/roomService.ts";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import AuthContext from "@/context/AuthContext";
+import IRoom from "@/interfaces/IRoom";
 
 export function NavMain() {
-    const [rooms, setRooms] = useState<roomI[]>([]);
-    const navigate = useNavigate();
+  const [rooms, setRooms] = useState<IRoom[]>([]);
+  const [roomService] = useState(new RoomService());
+  const navigate = useNavigate();
+  const authContext = useContext(AuthContext);
 
-    useEffect(() => {
-        const fetchRooms = async () => {
-            try {
-                const service = new RoomService();
-                const result = await service.getAll();
-                if (result) setRooms(result);
-            } catch (err) {
-                console.error("Erreur chargement des rooms", err);
-            }
-        };
+  const fetchRooms = async () => {
+    try {
+      const result = await roomService.getAll();
+      const roomsUser = result.filter((room) =>
+        room.users?.some((u) => u._id === authContext?.user?._id)
+      );
 
-        fetchRooms();
-    }, []);
+      if (roomsUser) setRooms(roomsUser);
+    } catch (err) {
+      console.error("Erreur chargement des rooms", err);
+    }
+  };
+  useEffect(() => {
+    fetchRooms();
+  }, []);
 
-    return (
-        <SidebarGroup>
-            <SidebarGroupLabel>Salons</SidebarGroupLabel>
-            <SidebarMenu>
-                {rooms.map((room) => (
-                    <SidebarMenuItem key={room._id}>
-                        <SidebarMenuButton
-                            tooltip={room.game}
-                            onClick={() => navigate(`/rooms/${room._id}`)}
-                            className="flex items-center cursor-pointer">
-                            <Gamepad2 className="mr-2 h-4 w-4"/>
-                            <span className="leading-none">{room.game}</span>
-                        </SidebarMenuButton>
-                    </SidebarMenuItem>
-                ))}
-            </SidebarMenu>
-        </SidebarGroup>
-    );
+  return (
+    <SidebarGroup>
+      <SidebarGroupLabel>Salons</SidebarGroupLabel>
+      <SidebarMenu>
+        {rooms.map((room) => (
+          <SidebarMenuItem key={room._id}>
+            <SidebarMenuButton
+              tooltip={room.game}
+              onClick={() => navigate(`/rooms/${room._id}`)}
+              className="flex items-center cursor-pointer"
+            >
+              <Gamepad2 className="mr-2 h-4 w-4" />
+              <span className="leading-none">{room.game}</span>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        ))}
+      </SidebarMenu>
+    </SidebarGroup>
+  );
 }
