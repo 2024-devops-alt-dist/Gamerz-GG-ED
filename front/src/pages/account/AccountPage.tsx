18c75bdba6c18128c732a6466a0c6f68dc0f7261
Gwen <gwendoline.garlet@live.fr>
2025-05-12 10:03:26 +0200

    Fix mainLayout

--- front/src/pages/account/AccountPage.tsx
+++ front/src/pages/account/AccountPage.tsx
@@ -1,57 +1,62 @@
 import { AppSidebar } from "@/pages/home/component/AppSidebar";
-import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 import { Separator } from "@/components/ui/separator";
 import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbList,
-    BreadcrumbSeparator
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 import { useAuth } from "@/context/AuthContext";
 import Account from "@/pages/account/components/Account.tsx";
 
 export default function AccountPage() {
-    const { user, loading } = useAuth();
+  const { user, loading } = useAuth();
 
-    return (
-        <SidebarProvider>
-            <AppSidebar />
-            <SidebarInset>
-                <header
-                    className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear bg-[#2C2F33] text-white shadow-md px-4">
-                    <div className="flex items-center gap-2">
-                        <SidebarTrigger className="-ml-1" />
-                        <Separator orientation="vertical" className="mr-2 h-4 border-gray-500" />
-                        <Breadcrumb>
-                            <BreadcrumbList className="flex items-center gap-2">
-                                <BreadcrumbSeparator className="hidden md:block text-gray-400" />
-                                <BreadcrumbItem>
-                                    <BreadcrumbLink href="/"
-                                                    className="text-white font-semibold px-3 py-1 rounded-md hover:bg-gray-700 transition-all">
-                                        Accueil
-                                    </BreadcrumbLink>
-                                </BreadcrumbItem>
-                                <BreadcrumbSeparator />
-                                <BreadcrumbItem>
-                                    <BreadcrumbLink
-                                        className="text-white font-semibold px-3 py-1 rounded-md hover:bg-gray-700 transition-all">
-                                        Mon compte
-                                    </BreadcrumbLink>
-                                </BreadcrumbItem>
-                            </BreadcrumbList>
-                        </Breadcrumb>
-                    </div>
-                </header>
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear bg-[#2C2F33] text-white shadow-md px-4">
+          <div className="flex items-center gap-2">
+            <SidebarTrigger className="-ml-1" />
+            <Separator
+              orientation="vertical"
+              className="mr-2 h-4 border-gray-500"
+            />
+            <Breadcrumb>
+              <BreadcrumbList className="flex items-center gap-2">
+                <BreadcrumbSeparator className="hidden md:block text-gray-400" />
+                <BreadcrumbItem>
+                  <BreadcrumbLink
+                    href="/"
+                    className="text-white font-semibold px-3 py-1 rounded-md hover:bg-gray-700 transition-all"
+                  >
+                    Accueil
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                  <BreadcrumbLink className="text-white font-semibold px-3 py-1 rounded-md hover:bg-gray-700 transition-all">
+                    Mon compte
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
 
-                <div className="p-6 bg-[#23272A] min-h-screen text-white">
-                    {loading && <div>Chargement utilisateur...</div>}
-                    {!loading && !user && <div>Vous devez être connecté</div>}
-                    {!loading && user && (
-                        <Account />
-                    )}
-                </div>
-            </SidebarInset>
-        </SidebarProvider>
-    );
+        <div className="p-6 bg-[#23272A] min-h-screen text-white">
+          {loading && <div>Chargement utilisateur...</div>}
+          {!loading && !user && <div>Vous devez être connecté</div>}
+          {!loading && user && <Account />}
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
 }
--- front/src/pages/admin/Admin.tsx
+++ front/src/pages/admin/Admin.tsx
@@ -1,65 +1,23 @@
-import AppSidebarAdmin from "@/components/sideBar/AppSidebarAdmin";
-import { Separator } from "@/components/ui/separator";
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
 import { ReactElement, useState } from "react";
-import { Button } from "@/components/ui/button";
-import { useNavigate } from "react-router-dom";
+import AppSidebarAdmin from "@/components/sideBar/AppSidebarAdmin";
 
-const Admin = () => {
+const AdminWrapper = () => {
   const [activeComponent, setActiveComponent] = useState<ReactElement | null>(
     null
   );
 
-  const [isFading, setIsFading] = useState(false);
-  const navigate = useNavigate();
-
-  const handleGoHome = () => {
-    setIsFading(true);
-    setTimeout(() => {
-      navigate("/");
-    }, 300);
-  };
-
   return (
-    <SidebarProvider>
+    <>
       <AppSidebarAdmin onSelectComponent={setActiveComponent} />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b">
-          <div className="flex items-center gap-2 px-3">
-            <SidebarTrigger />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-          </div>
-          <Button
-            onClick={handleGoHome}
-            variant="outline"
-            className="text-sm transition-all duration-200 hover:scale-[1.02] hover:bg-primary/10"
-          >
-            Accueil
-          </Button>
-        </header>
-        <div
-          className={`flex flex-1 flex-col gap-4 p-4 min-h-[100vh] rounded-xl bg-muted/50 md:min-h-min transition-opacity duration-300 ${
-            isFading ? "opacity-0" : "opacity-100"
-          }`}
-        >
-          {activeComponent ? (
-            activeComponent
-          ) : (
-            <p>Sélectionnez un élément du menu</p>
-          )}
-          {/* <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-          </div> */}
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
+      <div className="flex flex-1 flex-col gap-4">
+        {activeComponent ? (
+          activeComponent
+        ) : (
+          <p>Sélectionnez un élément du menu</p>
+        )}
+      </div>
+    </>
   );
 };
 
-export default Admin;
+export default AdminWrapper;
